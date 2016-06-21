7036869186143c55da712bb534cf00099a55a6a3
Tomas Molina <tamolinamendoza@gmail.com>
2016-06-21 02:21:49 +0000

    Done.

--- ginny_invitation.rb
+++ ginny_invitation.rb
@@ -1,6 +1,6 @@
 name = "ginny".capitalize
 day = "sunday".capitalize
-date = "17th"
-year ="1998"
+date = "17th".to_i
+year ="1998".to_i
 invitation = "The family of #{name} Weasley proudly invite you to their graduation commencement on #{day} the #{date} of May #{year}. Festivities will be held at The Burrow. See you then!"
 puts invitation
